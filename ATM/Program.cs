1b84b822dc787f62b98107b19da61c4e2953d586
serenakang <serenakang77@gmail.com>
2023-02-16 21:29:46 -0500

    Added method in main

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -39,4 +39,47 @@ public class cardHolder
     {
         return balance;
     }
+
+    public void setNum(String newCardNum)
+    {
+        cardnum = newCardNum;
+    }
+
+    public void setPin(int newPin)
+    {
+        pin = newPin;   
+    }
+
+    public void setFirstName(String newFirstName) {
+        firstName= newFirstName;
+    }
+
+    public void setLastName(String newLastName) {
+        lastName= newLastName;
+    }
+
+    public void setBalance(double newBalance) {
+        balance = newBalance;
+    }
+
+    public static void Main(String[] args)
+    {
+        void printOptions()
+        {
+            Console.WriteLine("Please choose from one of the following options...");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+        }
+
+        void deposit(cardHolder currentUser)
+        {
+            Console.WriteLine("How much money would you like to deposit? ");
+            double deposit = Double.Parse(Console.ReadLine());
+            currentUser.setBalance(deposit);
+            Console.WriteLine("Thank you for your $$. Your new balance is: " + currentUser.getBalance());
+        }
+    }
+
 }
